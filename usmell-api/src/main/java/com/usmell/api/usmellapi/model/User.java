3516ae61eb94ddb735fd0bf97397b4ee483bc300
Alec Kovalczik <adk4802@g.rit.edu>
2023-11-04 18:17:12 -0400

    basic user class created

--- usmell-api/src/main/java/com/usmell/api/usmellapi/model/User.java
+++ usmell-api/src/main/java/com/usmell/api/usmellapi/model/User.java
@@ -1,5 +1,33 @@
 package com.usmell.api.usmellapi.model;
 
 public class User {
-    
+    private static int nextID;
+    private int userID;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.userID = nextID;
+        nextID++;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
 }
