d539f1b6328bc8438faa4fc5ef6eeb148bca8b6c
Alec Kovalczik <adk4802@g.rit.edu>
2023-11-04 16:30:10 -0400

    Smell class drafted

--- usmell-api/src/main/java/com/usmell/api/usmellapi/model/Smell.java
+++ usmell-api/src/main/java/com/usmell/api/usmellapi/model/Smell.java
@@ -7,17 +7,43 @@ public class Smell {
     private int smellID;
     private String name;
     private double rating = 0.0;
-    private HashMap<String, Review> reviews; // <userID, Review>
+    private HashMap<Integer, Review> reviews = null; // <userID, Review>
     private SmellType smellType;
     
-    public Smell(String name, SmellType smellType){
-        setID();
+    public Smell(String name, SmellType smellType, int userID, int originalRating, String comment){
         this.name = name;
         this.smellType = smellType;
+        createReview(userID, rating);
+
+        // Set the id to a unique int using a static variable
+        this.smellID = nextSmellID;
+        smellID++;
     }
 
-    private void setID(){
-        this.smellID = nextSmellID;
-        nextSmellID++;
+    public int getReportedCount(){
+        return reviews.size();
+    }
+
+    public double getRating(){
+        return this.rating;
+    }
+
+    public void createReview(int userID, int originalRating, String comment){
+        Review review = new Review(userID, comment, originalRating);
+        if (reviews.containsKey(userID)){
+            throw new Exception("User already reviewed this smell");
+        }
+        this.reviews.put(userID, review);
+    }
+
+    public void editReview(int userID, int originalRating, String comment){
+        Review review = new Review(userID, comment, originalRating);
+        if (reviews.containsKey(userID)){
+            this.reviews.replace(userID, review);
+        }
+    }
+
+    public void removeReview(int userID){
+        this.reviews.remove(userID);
     }
 }
