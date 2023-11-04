8a0d59946de931da62d3d338edae30305eea7bb7
Alec Kovalczik <adk4802@g.rit.edu>
2023-11-04 16:07:59 -0400

    created the SmellType enums

--- usmell-api/src/main/java/com/usmell/api/usmellapi/model/Smell.java
+++ usmell-api/src/main/java/com/usmell/api/usmellapi/model/Smell.java
@@ -1,5 +1,23 @@
 package com.usmell.api.usmellapi.model;
 
+import java.util.HashMap;
+
 public class Smell {
+    private static int nextSmellID;
+    private int smellID;
+    private String name;
+    private double rating = 0.0;
+    private HashMap<String, Review> reviews; // <userID, Review>
+    private SmellType smellType;
     
+    public Smell(String name, SmellType smellType){
+        setID();
+        this.name = name;
+        this.smellType = smellType;
+    }
+
+    private void setID(){
+        this.smellID = nextSmellID;
+        nextSmellID++;
+    }
 }
