b708ee2ff631d17b0a992f3caadbfadb298aa790
Alec Kovalczik <adk4802@g.rit.edu>
2023-11-04 13:23:15 -0400

    Started making the file structure

---
+++ usmell-api/src/main/java/com/usmell/api/usmellapi/USmellApiApplication.java
@@ -0,0 +1,14 @@
+package com.usmell.api.usmellapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class USmellApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(USmellApiApplication.class, args);
+	}
+
+}
+
